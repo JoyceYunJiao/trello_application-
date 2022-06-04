e83185b81f337db3191c26a1f2d5874cf7841fe5
Trent <trentthebaker@gmail.com>
2022-06-04 14:18:47 -0300

    Skeleton for User management and login info

---
+++ backend/src/main/java/group25/usermanagement/User.java
@@ -0,0 +1,10 @@
+package group25.usermanagement;
+
+public class User {
+    private int id;
+    private String email;
+    private String fname;
+    private String lname;
+    private String password;
+    private String username;
+}

3a2e5be6e2e4b5e2f119dc8161a3591baa87996d
Trent <trentthebaker@gmail.com>
2022-06-17 20:15:42 -0300

    Started functionality for retrieving assigned users (not working yet)

--- backend/src/main/java/group25/group25/workspace/controller/WorkspaceController.java
+++ backend/src/main/java/group25/group25/workspace/controller/WorkspaceController.java
@@ -1,17 +1,29 @@
 package group25.group25.workspace.controller;
 
+import group25.group25.usermanagement.model.User;
 import group25.group25.workspace.model.UserAccessWorkspace;
 import group25.group25.workspace.model.Workspace;
+import group25.group25.workspace.repository.WorkspaceRepository;
 import group25.group25.workspace.service.WorkspaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class WorkspaceController {
     @Autowired
     WorkspaceService workspaceService;
+    @Autowired
+    WorkspaceRepository workspaceRepository;
+
+    @PostMapping(path = "/getAssignedUsers", consumes = "application/json", produces = "application/json")
+    public List<User> getAssignedUsers(@RequestBody Workspace workspace) {
+        return workspaceRepository.assignedUsers(workspace.getId());
+    }
 
     @PostMapping(path = "/addWorkspace", consumes = "application/json", produces = "application/json")
     public String addWorkspace(@RequestBody Workspace workspace) {
--- backend/src/main/java/group25/group25/workspace/repository/WorkspaceRepository.java
+++ backend/src/main/java/group25/group25/workspace/repository/WorkspaceRepository.java
@@ -1,11 +1,18 @@
 package group25.group25.workspace.repository;
 
+import group25.group25.usermanagement.model.User;
 import group25.group25.workspace.model.Workspace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace, Integer> {
+    //TODO: This does not work for some reason
+    @Query(value = "SELECT u FROM User u INNER JOIN UserAccessWorkspace a WHERE a.workspaceId = :id")
+    public List<User> assignedUsers(@Param(value = "id") int workspaceId);
 }

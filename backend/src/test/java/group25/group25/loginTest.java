553a50a79365a8381b4f25bd3ee6625faa329e4d
Songbo Niu <sn629057@da.ca>
2022-07-06 07:33:58 -0300

    logintest

---
+++ backend/src/test/java/group25/group25/loginTest.java
@@ -0,0 +1,38 @@
+package group25.group25;
+
+import group25.group25.usermanagement.model.User;
+import group25.group25.usermanagement.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import group25.group25.usermanagement.serviceImplementation.UserServices;
+
+@SpringBootTest
+public class loginTest {
+
+    @Autowired
+    UserServices UserServices;
+    @Autowired
+    UserRepository UserRepository;
+
+
+    @Test
+    void validLogin(){
+
+        User user = new User("test@email.com", "Tom", "Scott", "rrr", "doweevenneedthisfield");
+
+
+        Assertions.assertTrue(UserServices.login("test@email.com","rrr").getEmail().equals(user.getEmail()));
+        Assertions.assertTrue(UserServices.login("test@email.com","rrr").getPassword().equals(user.getPassword()));
+        Assertions.assertTrue(UserServices.login("test@email.com","rrr").getFirstName().equals(user.getFirstName()));
+        Assertions.assertTrue(UserServices.login("test@email.com","rrr").getUsername().equals(user.getUsername()));
+
+        Assertions.assertNull(UserServices.login("test@email.com","bsd"));
+
+
+    }
+
+
+
+}
---
+++ backend/src/test/java/group25/group25/usermanagement/serviceImplementation/UserServicesTest.java
@@ -0,0 +1,12 @@
+package group25.group25.usermanagement.serviceImplementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServicesTest {
+
+    @Test
+    void login() {
+    }
+}

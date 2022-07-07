bc83343079c7d1dfcc80afd3c3d2aacfcd31ae71
Trent <trentthebaker@gmail.com>
2022-07-07 00:43:16 -0300

    Overhauled user access workspace backend

--- backend/src/main/java/group25/group25/usermanagement/controller/UserController.java
+++ backend/src/main/java/group25/group25/usermanagement/controller/UserController.java
@@ -3,6 +3,7 @@ package group25.group25.usermanagement.controller;
 import group25.group25.usermanagement.model.User;
 import group25.group25.usermanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,15 @@ public class UserController {
     @RequestMapping(value = "getusers", method = RequestMethod.GET)
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @RequestMapping(value = "getUser/{id}", method = RequestMethod.GET)
+    public User getUserById(@PathVariable("id") int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @RequestMapping(value = "getUserByEmail/{email}", method = RequestMethod.GET)
+    public List<User> getUserByEmail(@PathVariable("email") String email) {
+        return userRepository.findByEmail(email);
     }
 }

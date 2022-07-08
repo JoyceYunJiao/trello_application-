98a0dd649f7de3981714bb65f2df1cf71903cc6b
Trent <trentthebaker@gmail.com>
2022-07-08 00:08:32 -0300

    Workspaces only show those relevent to logged in user

--- frontend/src/pages/Workspaces.tsx
+++ frontend/src/pages/Workspaces.tsx
@@ -1,5 +1,7 @@
+import { AlertTitle } from "@mui/material";
 import axios from "axios";
 import React, {useState, useEffect} from "react";
+import { Alert } from "react-bootstrap";
 import ViewWorkspaces from "../components/workspaces/ViewWorkspaces";
 
 function Workspaces() {
@@ -10,13 +12,39 @@ function Workspaces() {
         axios.get("http://localhost:8080/getAllWorkspaces")
             .then(response => {
                 console.log("Getting something");
-                setWorkspaceData(response.data);
+
+                const user = localStorage.getItem("user");
+
+                if (user === null) {
+                    setWorkspaceData([]);
+                    return;
+                }
+
+                const userData = JSON.parse(user);
+                const userWorkspaces = userData.workspaces;
+
+                const workspaces = response.data;
+                
+                // Get only workspaces in common with user
+                const filteredWorkspaces:any = Array();
+                for (const workspace of workspaces) {
+                    for (const userWorkspace of userWorkspaces) {
+                        if (workspace.id === userWorkspace.id) {
+                            filteredWorkspaces.push(workspace);
+                        }
+                    }
+                }
+
+                setWorkspaceData(filteredWorkspaces);
             });
     }
 
     useEffect(() => {
         getAllWorkspaces();
     }, []);
+
+    if (workspaceData.length === 0)
+        return <p>No workspaces to show. If you are logged in, you can create a new workspace using the create workspace page found in the top bar.</p>;
 
     return (
         <div>

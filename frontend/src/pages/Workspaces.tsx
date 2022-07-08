053b057002299d9afb0e9e9162e05d7173f2fd94
Trent <trentthebaker@gmail.com>
2022-07-08 12:56:45 -0300

    Workspaces page finally functional

--- frontend/src/pages/Workspaces.tsx
+++ frontend/src/pages/Workspaces.tsx
@@ -8,42 +8,31 @@ function Workspaces() {
 
     const [workspaceData, setWorkspaceData] = useState([]);
 
+    const userString = localStorage.getItem("user");
+    const user = (userString===null)? null : JSON.parse(userString);
+
     function getAllWorkspaces() {
-        axios.get("http://localhost:8080/getAllWorkspaces")
+        axios.get("http://localhost:8080/getWorkspaces/" + user.id)
             .then(response => {
                 console.log("Getting something");
 
-                const user = localStorage.getItem("user");
 
-                if (user === null) {
+                if (user === null || user === undefined || user === "") {
                     setWorkspaceData([]);
                     return;
                 }
 
-                const userData = JSON.parse(user);
-                const userWorkspaces = userData.workspaces;
-
                 const workspaces = response.data;
-                
-                // Get only workspaces in common with user
-                const filteredWorkspaces:any = Array();
-                for (const workspace of workspaces) {
-                    for (const userWorkspace of userWorkspaces) {
-                        if (workspace.id === userWorkspace.id) {
-                            filteredWorkspaces.push(workspace);
-                        }
-                    }
-                }
-
-                setWorkspaceData(filteredWorkspaces);
+                setWorkspaceData(workspaces);
             });
     }
 
     useEffect(() => {
-        getAllWorkspaces();
+        if (user !== null)
+            getAllWorkspaces();
     }, []);
 
-    if (workspaceData.length === 0)
+    if (workspaceData === null || workspaceData.length === 0)
         return <p>No workspaces to show. If you are logged in, you can create a new workspace using the create workspace page found in the top bar.</p>;
 
     return (
